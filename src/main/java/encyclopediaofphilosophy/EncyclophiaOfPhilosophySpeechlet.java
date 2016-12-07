df65701ad14e6e75b8316023eac93bdaac632c39
karlnicholas <karl.nicholas@outlook.com>
2016-12-07 12:24:33 -0800

    Cleanup

--- src/main/java/encyclopediaofphilosophy/EncyclophiaOfPhilosophySpeechlet.java
+++ src/main/java/encyclopediaofphilosophy/EncyclophiaOfPhilosophySpeechlet.java
@@ -9,13 +9,8 @@
  */
 package encyclopediaofphilosophy;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.nio.charset.Charset;
 
-import org.apache.commons.io.IOUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
